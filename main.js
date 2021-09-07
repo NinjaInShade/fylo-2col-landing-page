283dd1204dd4c3c24de31689ab419fc70a3d96a2
Leon Michalak <leonmichalak6@gmail.com>
2021-09-07 16:40:25 +0100

    Subscribe form section complete

--- main.js
+++ main.js
@@ -19,3 +19,25 @@ function submitHeaderForm(e) {
     headerError.innerText = 'Please check your email';
   }
 }
+
+function submitSubscribeForm(e) {
+  e.preventDefault();
+
+  const subscribeForm = document.getElementById('subscribe-form');
+  const subscribeInput = subscribeForm.querySelector('input');
+  const subscribeError = subscribeForm.querySelector('p');
+
+  subscribeInput.classList.remove('subscribe-form-input-error');
+  subscribeError.classList.add('hidden');
+  subscribeError.innerText = 'Form is valid';
+
+  const email_re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  // Form is invalid
+  if (!subscribeInput.value || !email_re.test(String(subscribeInput.value).toLowerCase())) {
+    subscribeInput.classList.add('subscribe-form-input-error');
+    subscribeError.classList.remove('hidden');
+    subscribeError.innerText = 'Please check your email';
+  }
+}
